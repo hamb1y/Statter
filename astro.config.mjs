cea008640ac4299479bcc27b2433e7a41e3f7702
hamb1y <rishi.s.malnad@gmail.com>
2025-11-01 15:39:17 +0530

    did some shit now its working yay! :D

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'path';
+import svelte from '@astrojs/svelte';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [svelte(), tailwind({ applyBaseStyles: false })],
+	vite: {
+		resolve: { alias: { $lib: path.resolve('./src/lib') } },
+		ssr: { noExternal: ['lucide-svelte'] }
+	}
+});
